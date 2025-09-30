00fd14efbea9df9a4d39fb657ef4f3a927b59b43
Bochkov Denis <dybochkov@mts.ru>
2025-09-30 22:59:06 +0400

    added articles sort

--- apps/articles-app/src/routes/article.$articleId.tsx
+++ apps/articles-app/src/routes/article.$articleId.tsx
@@ -18,7 +18,7 @@ function Article() {
 
   return <div className="container mx-auto px-4 py-4">
     <Link to="/" className="text-blue-500 hover:underline mb-4">
-      <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
+      <svg className="w-6 h-6 text-gray-800 dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"></path>
       </svg>
     </Link>
